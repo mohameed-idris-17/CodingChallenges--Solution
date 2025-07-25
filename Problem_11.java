76b8cdd79777a07b94b951cb77c5ed2c9986d016
MOHAMEED IDRIS M <idrisvolley17@gmail.com>
2025-07-25 23:05:31 +0530

    Create Problem_11.java

---
+++ Problem_11.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class Hello {
+
+public static void main(String[] args) {
+
+Scanner sc = new Scanner(System.in);
+
+int a = sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();
+
+int[] arr = new int[15];
+
+arr[0] = a; arr[1] =b; arr [2] = C
+
+if(a!=0){
+
+}
+
+arr[3] = a+10+b;
+
+arr[4]= a*10+c;
+
+arr[9]= a*100+b*10+c;
+
+arr[12] = a*100+c*10+b;
+
+if(b! = 0)
+
+}
+
+arr[5] =b^ * 10 + c
+
+arr[8] = b + 1theta + a
+
+arr[11] = b + 100 +c^ * 10+a
+
+arr[10] = b + 100 +a^ * 10+c
+
+if(c!=0){
+
+arr[6] =c^ prime prime 10 + b ;
+
+arr[7] = c + 10 + a
+
+arr[13] c*100+b*10+a;
+
+arr[14] = c + 100 + a ^ 10 + b
+
+}
+
+Arrays.sort(arr);
+
+for(int charr) {
+
+boolean bool = true;
+
+if(ch<=1){
+
+bool = false;
+
+}else{
+
+for(int i = 2 i<Math.sqrt(ch);i++){
+
+if((ch%i)==0){
+
+bool false;
+
+I
+
+break;
+
+}
+
+}}
+
+if(bool) {
+
+System.out.print(ch+" ");
