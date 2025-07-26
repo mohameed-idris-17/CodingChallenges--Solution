87d378016166e36184941b23d11729d31c28ece4
MOHAMEED IDRIS M <idrisvolley17@gmail.com>
2025-07-26 23:12:31 +0530

    Create Problem_13.java

---
+++ Problem_13.java
@@ -0,0 +1,11 @@
+public class Problem_13{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+    if(number%2==0)
+      System.out.println("even");
+    else 
+     System.out.println("odd");
+
+}
+}
