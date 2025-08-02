d6bfbabf66a6c685e5af3c66e743cfbaa7f1bbf8
MOHAMEED IDRIS M <idrisvolley17@gmail.com>
2025-08-02 23:29:52 +0530

    program structure update

--- Problem_23.java
+++ Problem_23.java
@@ -1,35 +1,39 @@
-import java.util.*; public class Problem_23{
+import java.util.*; 
+  public class Problem_23{
+    public static void main(String[] args)
+       {
+   int n = new Scanner(System.in).nextInt(); 
+    char[][] mat = new char[n*n] [n*n];
 
-T
+    for(int i=0;i<n*n;i++){
 
-public static void main(String[] args) { int n = new Scanner(System.in).nextInt(); char[][] mat = new char[n*n] [n*n];
+        if(i%2==0)
+          {
+               mat[0][i]='*';
+      }
+  
 
-for(int i=0;i<n*n;i++){
+      else{
 
-}
+            mat[0][i]='-';
 
-}
+      }
 
-if(i%2==0){ mat[0][i]='*';
+      }
 
-}
 
-else{
 
-mat[0][i]='-';
-
-}
-
-}
-
-}
-
-for(int i=1;i<n*n;i++){ mat[i][0] = mat[i-1][n*n-1]; for(int j=0;j<n*n-1;j++){ mat[i][j+1] = mat[i-1][j];
-
-}
-
-for(int i=0;i<n*n;i++){ for(int j=0;j<n*n;j++){ System.out.print(mat[i][j]+" ");
+for(int i=1;i<n*n;i++){ 
+    mat[i][0] = mat[i-1][n*n-1]; 
+  for(int j=0;j<n*n-1;j++){ 
+    mat[i][j+1] = mat[i-1][j];
 
 }
 
-System.out.println();
+for(int i=0;i<n*n;i++){ 
+      for(int j=0;j<n*n;j++){ 
+    System.out.print(mat[i][j]+" ");
+
+}
+
+System.out.println();}}}
