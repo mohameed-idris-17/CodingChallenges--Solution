82a9969ab82b51e6391139d5b365f7e42ab49d7d
MOHAMEED IDRIS M <idrisvolley17@gmail.com>
2025-08-13 22:25:27 +0530

    Binary_Fibonacci_series

---
+++ Binary_Fibonacci_series.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+1 = []
+
+f1 = "0"
+
+f2 ="1"
+
+print(f1)
+
+print(f2)
+
+1.append(f1)
+
+1.append(f2)
+
+for i in range(n-2):
+
+f3 = f1+f2
+
+print(f3)
+
+1.append(f3)
+
+f1 = f2
+
+f2 = f3
+
+for j in range(n-1,0,-1): print(1[j])
